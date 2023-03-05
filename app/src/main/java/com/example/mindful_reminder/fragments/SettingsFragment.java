818eaf48550113b297c9687eeff69f7bb7270e0d
way-2 <dlw1165@gmail.com>
2023-03-05 14:39:52 -0500

    Made notification interval configurable

--- app/src/main/java/com/example/mindful_reminder/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/mindful_reminder/fragments/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.example.mindful_reminder.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -8,24 +10,33 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 import androidx.preference.SwitchPreferenceCompat;
 import androidx.work.Constraints;
 import androidx.work.ExistingPeriodicWorkPolicy;
 import androidx.work.NetworkType;
 import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
 import com.example.mindful_reminder.R;
 import com.example.mindful_reminder.service.NotificationWorker;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
-    private static SwitchPreferenceCompat notificationSwitch;
+    private SwitchPreferenceCompat notificationSwitch;
+    private ListPreference notificationIntervalListPreference;
+    private SharedPreferences sharedPreferences;
     private static final String NOTIFICATION_TOGGLE_SWITCH_KEY = "notification_toggle";
+    private static final String NOTIFICATION_TIME_INTERVAL_LIST = "notification_time_interval_list";
+    private static final String NOTIFICATION_INTERVAL_LONG = "notification_interval_long";
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,6 +46,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext());
     }
 
     @NonNull
@@ -69,6 +81,37 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 return true;
             }
         });
+        notificationIntervalListPreference = (ListPreference) findPreference(NOTIFICATION_TIME_INTERVAL_LIST);
+        if (!sharedPreferences.contains(NOTIFICATION_INTERVAL_LONG)) {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putLong(NOTIFICATION_INTERVAL_LONG, Long.parseLong(notificationIntervalListPreference.getValue()));
+            editor.apply();
+        }
+        notificationIntervalListPreference.setSummary(notificationIntervalListPreference.getEntry());
+        notificationIntervalListPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(@NonNull Preference preference, Object newValue) {
+                String textValue = newValue.toString();
+                int index = notificationIntervalListPreference.findIndexOfValue(textValue);
+                String entryString = notificationIntervalListPreference.getEntries()[index].toString();
+                String entryValue = notificationIntervalListPreference.getEntryValues()[index].toString();
+                long longValue = Long.parseLong(entryValue);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putLong(NOTIFICATION_INTERVAL_LONG, longValue);
+                editor.apply();
+                notificationIntervalListPreference.setSummary(entryString);
+                try {
+                    List<WorkInfo> workInfo = WorkManager.getInstance(requireContext()).getWorkInfosByTag(NotificationWorker.NOTIFICATION_WORKER_TAG).get();
+                    WorkInfo.State state = workInfo.get(0).getState();
+                    if ((state == WorkInfo.State.RUNNING) || (state == WorkInfo.State.ENQUEUED)) {
+                        startNotificationWorker();
+                    }
+                } catch (ExecutionException | InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+                return true;
+            }
+        });
     }
 
     private void stopNotificationWorker() {
@@ -77,7 +120,9 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     }
 
     private void startNotificationWorker() {
-        PeriodicWorkRequest.Builder workBuilder = new PeriodicWorkRequest.Builder(NotificationWorker.class, 30, TimeUnit.MINUTES).setInitialDelay(15, TimeUnit.SECONDS).addTag(NotificationWorker.NOTIFICATION_WORKER_TAG);
+        long notificationInterval = sharedPreferences.getLong(NOTIFICATION_INTERVAL_LONG, 30);
+        Log.i("notificationSetting", "Setting notifications to every " + notificationInterval + " minutes");
+        PeriodicWorkRequest.Builder workBuilder = new PeriodicWorkRequest.Builder(NotificationWorker.class, notificationInterval, TimeUnit.MINUTES).setInitialDelay(15, TimeUnit.SECONDS).addTag(NotificationWorker.NOTIFICATION_WORKER_TAG);
         Constraints constraints = new Constraints.Builder()
                 .setRequiredNetworkType(NetworkType.CONNECTED)
                 .setRequiresCharging(false)
