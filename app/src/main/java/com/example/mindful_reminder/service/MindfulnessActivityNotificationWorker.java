adc6cba4339b742222b8ebf409c9c3b35f5999dc
way-2 <dlw1165@gmail.com>
2023-03-13 20:36:31 -0400

    may have fixed the missing notifications

--- app/src/main/java/com/example/mindful_reminder/service/MindfulnessActivityNotificationWorker.java
+++ app/src/main/java/com/example/mindful_reminder/service/MindfulnessActivityNotificationWorker.java
@@ -1,6 +1,5 @@
 package com.example.mindful_reminder.service;
 
-import static com.example.mindful_reminder.activities.ActivityMain.AFFIRMATION_SHARED_PREFERENCE;
 import static com.example.mindful_reminder.activities.ActivityMain.DAILY_MINDFULNESS_ACTIVITY_SHARED_PREFERENCE;
 
 import android.Manifest;
@@ -9,8 +8,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.provider.Settings;
-import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -39,29 +36,20 @@ public class MindfulnessActivityNotificationWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        int dndStatus = 0;
-        try {
-            dndStatus = Settings.Global.getInt(getApplicationContext().getContentResolver(), "zen_mode");
-        } catch (Settings.SettingNotFoundException ex) {
-            Log.w(ACTIVITY_NOTIFICATION_WORKER_TAG, "Unable to find zen_mode status");
-        }
-        if (dndStatus == 0) {
-            Log.i(ACTIVITY_NOTIFICATION_WORKER_TAG, "Sending Notification for affirmation " + sharedPreferences.getString(AFFIRMATION_SHARED_PREFERENCE, ""));
-            Intent intent = new Intent(getApplicationContext(), ActivityMain.class);
-            TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(getApplicationContext());
-            taskStackBuilder.addNextIntentWithParentStack(intent);
-            PendingIntent pendingIntent = taskStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), ActivityMain.NOTIFICATION_CHANNEL)
-                    .setSmallIcon(R.drawable.happy_brain)
-                    .setContentTitle(sharedPreferences.getString(DAILY_MINDFULNESS_ACTIVITY_SHARED_PREFERENCE, ""))
-                    .setContentIntent(pendingIntent)
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
-            if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(getApplicationContext(), "Notification Permission not granted", Toast.LENGTH_SHORT).show();
-            } else {
-                notificationManagerCompat.notify(new Random().nextInt(), builder.build());
-            }
+        Intent intent = new Intent(getApplicationContext(), ActivityMain.class);
+        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(getApplicationContext());
+        taskStackBuilder.addNextIntentWithParentStack(intent);
+        PendingIntent pendingIntent = taskStackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), ActivityMain.NOTIFICATION_CHANNEL)
+                .setSmallIcon(R.drawable.happy_brain)
+                .setContentTitle(sharedPreferences.getString(DAILY_MINDFULNESS_ACTIVITY_SHARED_PREFERENCE, ""))
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getApplicationContext(), "Notification Permission not granted", Toast.LENGTH_SHORT).show();
+        } else {
+            notificationManagerCompat.notify(new Random().nextInt(), builder.build());
         }
         return Result.success();
     }
